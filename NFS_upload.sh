ad0ad3213a39f1bfe28499b05ccc5abddcf57c23
DpaHbaLT <andrej.koza4enko@gmail.com>
2024-07-26 14:07:07 +0300

    update

--- NFS_upload.sh
+++ NFS_upload.sh
@@ -6,6 +6,27 @@ NFS_PATH="/path/on/nfs"
 MOUNT_POINT="/mnt/nfs"
 LOCAL_FILE="path/to/local/file"
 UPLOAD_FILE_NAME=$(basename ${LOCAL_FILE})
+HOSTNAME=$(hostname) # Ensure the hostname is defined
+
+# Define the UCS filename with hostname and a timestamp
+UCS_FILENAME="/var/local/ucs/${HOSTNAME}_backup_$(date +%F_%H-%M-%S).ucs"
+
+# Log the start of the backup process
+logger -p local0.info "System backup started on $HOSTNAME"
+
+# Save the system configuration
+tmsh save sys config
+
+# Save the system UCS file
+tmsh save sys ucs $UCS_FILENAME
+
+# Check if the UCS save was successful
+if [[ $? -ne 0 ]]; then
+  echo "Failed to save UCS file"
+  exit 1
+fi
+
+echo "Backup successful: $UCS_FILENAME"
 
 # Mount the NFS share
 sudo mount -t nfs ${NFS_SERVER}:${NFS_PATH} ${MOUNT_POINT}
@@ -17,7 +38,7 @@ if [[ $? -ne 0 ]]; then
 fi
 
 # Copy the file to the NFS mount point
-cp ${LOCAL_FILE} ${MOUNT_POINT}/${UPLOAD_FILE_NAME}
+cp ${UCS_FILENAME} ${MOUNT_POINT}/${UPLOAD_FILE_NAME}
 
 # Check if the copy was successful
 if [[ $? -ne 0 ]]; then
